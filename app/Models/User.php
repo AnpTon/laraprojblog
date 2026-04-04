691a9fd5d02b62cbc8505593e4023580fd95c10c
AnpTon <anpton0713@gmail.com>
2026-04-05 01:19:26 +0800

    Models

--- app/Models/User.php
+++ app/Models/User.php
@@ -34,4 +34,12 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }

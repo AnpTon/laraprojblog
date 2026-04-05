98965f679c8e26d74459f0ab295f0f035a243eaf
AnpTon <anpton0713@gmail.com>
2026-04-05 20:42:13 +0800

    Seeding & Initial Listing

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             CategorySeeder::class,
+            PostsSeeder::class,
         ]);
         
         User::create([
